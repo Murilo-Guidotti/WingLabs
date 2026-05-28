91f7ea8657321c9c12410ef2b50070bd4ddaf390
Murilo-Guidotti <muriloguidotti.tinfem2023@gmail.com>
2026-05-28 13:47:54 -0300

    changed the parse method in ReadDatFile() to a more efficient one

--- 3D Generator/src/FileReader.cs
+++ 3D Generator/src/FileReader.cs
@@ -1,63 +1,36 @@
+using System.Globalization;
+
 namespace FileReader
 {
-    class readDat
+    public struct Coordinate
     {
-        public static int _lineNumber = 1;
-        public static void readDatFile(string filePath)
-        {
-            try
-            {
-                using (StreamReader reader = new StreamReader(filePath))
-                {
-                    string? line;
-                    char[,] x = new char[20, 1001];
-                    char[,] y = new char[20, 1001];
-                    int count = 0;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        if(line.StartsWith("NACA"))
-                        {
-                            Console.WriteLine(line);
-                            _lineNumber++;
-                        } else
-                        {
-                            for(count = 0; line[count] != ' '; count++)
-                            {
-                                x[count,_lineNumber] = line[count];
-                            }
+        public double X;
+        public double Y;
+    }
 
-                            Console.WriteLine("Line Count: " + _lineNumber);
-                            Console.WriteLine("Line: " + line);
-                            Console.WriteLine("X: " + getXinIndex(count, _lineNumber, x) + "\n");
-                            _lineNumber++;
-                        }
-                    }
+    public class DatParser
+    {
+        public static List<Coordinate> ReadDatFile(string filePath)
+        {
+            var coordinates = new List<Coordinate>();
+
+            foreach(string line in File.ReadLines(filePath))
+            {
+                if (line.StartsWith("NACA", StringComparison.OrdinalIgnoreCase) || string.IsNullOrWhiteSpace(line))
+                continue;
+
+                string[] parts = line.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length >= 2)
+                {
+                    double x = double.Parse(parts[0], CultureInfo.InvariantCulture);
+                    double y = double.Parse(parts[1], CultureInfo.InvariantCulture);
+
+                    coordinates.Add(new Coordinate {X = x, Y = y});
                 }
             }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-            }        
-        }
 
-        private static string getXinIndex(int count, int lineNumber, char[,] x)
-        {
-            char[] chars = new char[count];
-            for (int j = 0; j < count; j++)
-            {
-                chars[j] = x[j,lineNumber];
-            }
-
-            return new string(chars);
-        }
-
-        private static char getDatX(int count, int lineNumber, char[,] x, string line)
-        {
-            for(count = 0; line[count] != ' '; count++)
-            {
-                x[count,_lineNumber] = line[count];
-            }
-            return x[count, lineNumber];
+            return coordinates;
         }
     }
 }
