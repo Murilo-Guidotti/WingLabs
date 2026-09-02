94c2bb75ae61d5619b0f508539d7f33ae0538c9b
Murilo-Guidotti <muriloguidotti13@gmail.com>
2026-09-02 00:54:54 -0300

    3D modelling is working (Still in Development)

--- 3D Generator/src/Aircraft_Wings/planeWings.cs
+++ 3D Generator/src/Aircraft_Wings/planeWings.cs
@@ -25,20 +25,20 @@ namespace WingMaker
             }
 
             // Wing Parameters (all of this needs to be parameters)
-            float fSpanMM           = 7450.0f;
-            float fRootChordMM      = 1570.0f;
-            float fTipChordMM       = 784.5f;
-            float fTipTwistDeg      = -15.0f;
-            float fWingSweepDeg     = 30.0f;
-            float fWingDihedralDeg  = 0.0f;
-            float fTipChanferDeg    = 0.0f;
+            float   fSpanMM           = 7450.0f;
+            float   fRootChordMM      = 1570.0f;
+            float   fTipChordMM       = 784.5f;
+            float   fTipTwistDeg      = -15.0f;
+            double  fWingSweepDeg     = 30.0f;
+            float   fWingDihedralDeg  = 0.0f;
+            float   fTipChanferDeg    = 0.0f;
 
             // MemoryUsageDebug(); // Memory Debugger
             LocalFrame oRootFrame = new LocalFrame();
             LocalFrame oTipFrame  = new LocalFrame()
                                         .oTranslate(new Vector3(fRootChordMM / 2, 0, fSpanMM))
-                                        .oRotate(fTipTwistDeg * MathF.PI / 180f, Vector3.UnitZ)
-                                        .oTranslate(new Vector3((float)Math.Tan((double) fWingSweepDeg * MathF.PI / 180f) * fSpanMM, 0, 0));
+                                        .oRotate((float) (fTipTwistDeg * Math.PI / 180f), Vector3.UnitZ)
+                                        .oTranslate(new Vector3((float) Math.Tan(fWingSweepDeg * Math.PI / 180.0f) * fSpanMM, 0, 0));
 
             List<Vector3> aRootSection = avecBuildSection(coordinates, oRootFrame, fRootChordMM);
             List<Vector3> aTipSection  = avecBuildSection(coordinates, oTipFrame, fTipChordMM);
@@ -48,6 +48,7 @@ namespace WingMaker
             AddLoftBetweenSections(ref oMesh, aRootSection, aTipSection);
             AddCap(ref oMesh, aRootSection, bFlip: true);
             AddCap(ref oMesh, aTipSection, bFlip: false);
+            CreateSections(fSpanMM);
 
 
             // Export the mesh in STL format
@@ -56,14 +57,38 @@ namespace WingMaker
             
             Console.WriteLine($"[DEBUG] Mesh gerado: {oMesh.nVertexCount()} vértices, {oMesh.nTriangleCount()} triângulos");
 
-            Voxels voxWing = new(oMesh);
-            // voxWing.voxSmoothen(7.5f);
+            // Voxels voxWing = new(oMesh);
+            // voxWing.voxSmoothen(5f);
 
-            Library.oViewer().Add(voxWing, 0);
+            // Library.oViewer().Add(voxWing, 0);
             
-            Console.WriteLine("[DEBUG] Asa adicionada ao viewer.\n");
-            
+            Console.WriteLine("[DEBUG] Asa adicionada ao viewer.\n");            
         }
+
+        // Ignore this function, it's for the future if mesh division is needed
+        static void CreateSections(Mesh oMesh)
+        {
+            BBox3 bBox = new BBox3();
+            bBox = oMesh.oBoundingBox();
+            
+            float fSectionSize;
+
+            fSectionSize = (bBox.vecSize().Z / 10.0f);
+            Console.WriteLine($"[DEBUG] Tamanho de seção: {fSectionSize} mm");
+
+        }
+
+        // This is the actual function that will create the sections of the wing, based on the parameters provided
+        static void CreateSections(float fSpanMM = 0, float fRootChordMM = 0, float fTipChordMM = 0, float fTipTwistDeg = 0, double fWingSweepDeg = 0, float fWingDihedralDeg = 0, float fTipChanferDeg = 0)
+        {
+            double fSectionSize = fSpanMM / 10.0f;
+            Console.WriteLine($"[DEBUG] Tamanho de seção: {fSectionSize} mm");
+
+            LocalFrame []oNFrame = new LocalFrame[(int) Math.Floor(fSpanMM / fSectionSize)];
+            Console.WriteLine($"[DEBUG] Quantidade de seções: {oNFrame.Length}");
+        }
+
+        static void CreateSectionsMehs(){}
 
         async static void MemoryUsageDebug()
         {
